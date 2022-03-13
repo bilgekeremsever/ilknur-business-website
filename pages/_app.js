ef1bc8c006a6d207f96de47c1859b0392b5aa949
Bilge <bilgekerems@gmail.com>
2022-03-13 23:58:44 +0300

    Add global context provider.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "../styles/globals.scss"
+import App from "next/app"
 import Head from "next/head"
 import Layout from "../layout/Layout"
+import { fetchAPI } from "../lib/adminPanelApi"
+import { GlobalContextWrapper } from "../lib/contexts/GlobalContext"
 
 function MyApp({ Component, pageProps }) {
+  const { globalData } = pageProps
   return (
     <>
       <Head>
@@ -10,11 +14,22 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <GlobalContextWrapper value={globalData}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GlobalContextWrapper>
     </>
   )
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext)
+
+  const globalRes = await fetchAPI("/global")
+
+  return { ...appProps, pageProps: { globalData: globalRes.data.attributes } }
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="tr">
-        <Head/>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
