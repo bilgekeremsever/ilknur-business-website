e66b5eada7371e7997d94fe294d6d5ac0484bed0
Bilge <bilgekerems@gmail.com>
2022-03-16 01:10:40 +0300

    Topbar - fix design, populate data.

--- components/header/Topbar.js
+++ components/header/Topbar.js
@@ -1,20 +1,27 @@
 import styles from "../../styles/header/topbar.module.scss"
+import useGlobalContext from "../../lib/hooks/useGlobalContext"
+
 const Topbar = () => {
+  const { email, telephone, telephoneText, storeLink, storeText } =
+    useGlobalContext()
+
   return (
     <div id="topbar">
-      <div
-        className={`container ${styles.topbarItems}`}>
+      <div className={`container ${styles.topbarItems}`}>
         <span className={styles.phrase}>Hemen Ulaşın</span>
-        <a href="tel:+90123456789" className={styles.phone}>
+        <a href={`tel:${telephone}`} className={styles.phone}>
           <i className="bi bi-telephone"></i>
-          <span>+905364753</span>
+          <span>{telephoneText}</span>
         </a>
-        <a href="mailto:example@gmail.com" className={styles.mail}>
+        <a href={`mailto:${email}`} className={styles.mail}>
           <i className="bi bi-envelope"></i>
-          <span>example@gmail.com</span> 
+          <span>{email}</span>
         </a>
 
-        <a href="#" className={styles.storeLink}>store link</a>
+        <a href={storeLink} className={styles.storeLink}>
+          <i className="bi bi-box-arrow-up-right"></i>
+          {storeText}
+        </a>
       </div>
     </div>
   )
