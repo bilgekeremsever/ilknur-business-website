9c4b3679fe00f9943db922ccf5700c7a5e89921c
Bilge <bilgekerems@gmail.com>
2022-03-17 00:02:22 +0300

    Add carousel & cta area designs to index page.

---
+++ components/BsCarousel.js
@@ -0,0 +1,36 @@
+import Image from "next/image"
+
+import { Carousel } from "react-bootstrap"
+
+const BsCarousel = () => {
+  return (
+    <Carousel variant="dark" className="">
+      <Carousel.Item>
+        <Image
+          src="/example-slider-img.jpg"
+          alt="slider-item"
+          layout="fill"
+        />
+
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="/example-slider-img.jpg"
+          alt="slider-item"
+          layout="fill"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default BsCarousel
---
+++ components/CtaArea.js
@@ -0,0 +1,17 @@
+const CtaArea = () => {
+  return (
+    <div className="cta-area">
+      <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+        <i className="bi bi-shop-window"></i>
+        Mağazaya Git
+      </a>
+      <div className="cta-area__splitter"></div>
+      <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+        <i className="bi bi-person-plus"></i>
+        Üye Ol
+      </a>
+    </div>
+  )
+}
+
+export default CtaArea
