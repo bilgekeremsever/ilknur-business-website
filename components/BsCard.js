6b6b16b46f25d6a8b88f5b477e9c4485aa5fb7ea
Bilge <bilgekerems@gmail.com>
2022-03-17 14:28:30 +0300

    Fix BsCard design. Add educations to homepage.

--- components/BsCard.js
+++ components/BsCard.js
@@ -1,18 +1,21 @@
 import Link from "next/link"
 import { Card, Button } from "react-bootstrap"
 
-const BsCard = () => {
+const BsCard = ({ cardFrame }) => {
   return (
-    <Card className="bs-card">
+    <Card className={`bs-card ${cardFrame ? "framed" : ""}`}>
       <Card.Img src="/example-product.jpg" />
       <Card.Body>
+        <p className="card-text">
+          <small className="text-muted">29.09.2021, Ilknur Sever</small>
+        </p>
         <Card.Title>Lorem Ipsum</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the cards content.
         </Card.Text>
         <Link href="/">
-          <a className="card-link">Devamını Oku <i className="bi bi-arrow-right"></i> </a>
+          <a className="card-link">Devamını Oku</a>
         </Link>
       </Card.Body>
     </Card>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
+import BsCard from "../components/BsCard"
 import BsCarousel from "../components/BsCarousel"
 import CallbackForm from "../components/CallbackForm"
 import CardsCarousel from "../components/CardsCarousel"
@@ -8,7 +9,7 @@ import PersonFeatures from "../components/PersonFeatures"
 
 export default function Home() {
   return (
-    <main>
+    <main className="home-page">
       <Head>
         <title>Ilknur Sever - Anasayfa</title>
       </Head>
@@ -21,7 +22,22 @@ export default function Home() {
         <CardsCarousel />
       </section>
       <section>
-        <CallbackForm/>
+        <CallbackForm />
+      </section>
+      <section>
+        <div className="container latest-educations">
+          <div className="row">
+            <div className="col-lg-4">
+              <BsCard cardFrame={true} />
+            </div>
+            <div className="col-lg-4">
+              <BsCard cardFrame={true} />
+            </div>
+            <div className="col-lg-4">
+              <BsCard cardFrame={true} />
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   )
