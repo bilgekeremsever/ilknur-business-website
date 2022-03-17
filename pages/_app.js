85eb653529f5b18b0994f2716a1974f3be77eaa7
Bilge <bilgekerems@gmail.com>
2022-03-17 14:57:23 +0300

    Fix adminPanelApi & adminPanelApiMedia
    Fix adminPanelApi exported func. name & adminPanelMedia import.

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/main.scss"
 import App from "next/app"
 import Head from "next/head"
 import Layout from "../layout/Layout"
-import { fetchAPI } from "../lib/adminPanelApi"
+import { fetchAdminPanelAPI } from "../lib/adminPanelApi"
 import { GlobalContextWrapper } from "../lib/contexts/GlobalContext"
 
 function MyApp({ Component, pageProps }) {
@@ -29,7 +29,7 @@ MyApp.getInitialProps = async (appContext) => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext)
 
-  const globalRes = await fetchAPI("/global")
+  const globalRes = await fetchAdminPanelAPI("/global")
 
   return { ...appProps, pageProps: { globalData: globalRes.data.attributes } }
 }
